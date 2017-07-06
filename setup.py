9642541dd12174312faffb6ab8f9362b6b378780
gdgyy <524135921@qq.com>
2017-07-06 09:45:47 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="redisqueue",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/borntyping/cookiecutter-pypackage-minimal",
 
     author="gdgyy",
